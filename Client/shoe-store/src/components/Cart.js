aa4513d564dc8ed13c7f50fcff950b8b89a172ff
Or Bar Califa <orbarkalifa@gmail.com>
2024-07-24 21:51:02 +0300

    fixes

--- Client/shoe-store/src/components/Cart.js
+++ Client/shoe-store/src/components/Cart.js
@@ -1,32 +1,34 @@
 import React, { useState } from 'react';
 import CartItem from './CartItem';
+import './Cart.css';
+
 const Cart = () => {
     const initialCart = [];
     const [cart, setCart] = useState(initialCart);
     const updateQuantity = (id, quantity) => {
         setCart(cart.map(item => (item.id === id ? { ...item, quantity } : item)));
-      };
-      const deleteItem = (id) => {
-        setCart(cart.filter(item => item.id !== id));
-      };
-      const totalPrice = cart.reduce((total, item) => total + item.price * item.quantity, 0);
-      return (
-        <div className="cart">
-          <h2>Shopping Cart</h2>
-          {cart.map(item => (
-            <CartItem
-              key={item.id}
-              item={item}
-              onUpdateQuantity={updateQuantity}
-              onDelete={deleteItem}
-            />
-          ))}
-          <div className="cart-total">
-            <strong>Total: </strong>
-            <span>${totalPrice.toFixed(2)}</span>
-          </div>
-        </div>
-      );
     };
-    
+    const deleteItem = (id) => {
+        setCart(cart.filter(item => item.id !== id));
+    };
+    const totalPrice = cart.reduce((total, item) => total + item.price * item.quantity, 0);
+    return (
+        <div className="cart">
+            <h2>Shopping Cart</h2>
+            {cart.map(item => (
+                <CartItem
+                    key={item.id}
+                    item={item}
+                    onUpdateQuantity={updateQuantity}
+                    onDelete={deleteItem}
+                />
+            ))}
+            <div className="cart-total">
+                <strong>Total: </strong>
+                <span>${totalPrice.toFixed(2)}</span>
+            </div>
+        </div>
+    );
+};
+
 export default Cart;
--- Client/shoe-store/src/components/Homepage.js
+++ Client/shoe-store/src/components/Homepage.js
@@ -1,10 +1,12 @@
 // src/components/Homepage.js
 import React from 'react';
+import Cart from './Cart.js';
 
 function Homepage() {
     return (
         <div>
             <h1>Welcome to the Homepage!</h1>
+            <Cart />
         </div>
     );
 }
